04567507c9ab6b1ab1c978a288c0d8e74e11be4f
ymchenliye <ymchenliye@gmail.com>
2016-06-17 21:05:59 +0800

    搭建完基本框架，可以使用客户端开始连接，不过不处理命令

---
+++ Makefile
@@ -0,0 +1,11 @@
+.PHONY:clean
+CC=gcc
+CFLAGS=-Wall -g
+BIN=miniftpd
+OBJS=main.o sysutil.o session.o ftpproto.o privparent.o
+$(BIN):$(OBJS)
+	$(CC) $(CFLAGS) $^ -o $@
+%.o:%.c
+	$(CC) $(CFLAGS) -c $< -o $@
+clean:
+	rm -f *.o $(BIN)
---
+++ sysutil.h
@@ -0,0 +1,26 @@
+#ifndef _SYS_UTIL_H_
+#define _SYS_UTIL_H_
+#include "common.h"
+
+int tcp_server(const char *host, unsigned short port);
+
+int getlocalip(char *ip);
+
+void activate_nonblock(int fd);
+void deactivate_nonblock(int fd);
+
+int read_timeout(int fd, unsigned int wait_seconds);
+int write_timeout(int fd, unsigned int wait_seconds);
+int accept_timeout(int fd, struct sockaddr_in *addr, unsigned int wait_seconds);
+int connect_timeout(int fd, struct sockaddr_in *addr, unsigned int wait_seconds);
+
+ssize_t readn(int fd, void *buf, size_t count);
+ssize_t writen(int fd, const void *buf, size_t count);
+ssize_t recv_peek(int sockfd, void *buf, size_t len);
+ssize_t readline(int sockfd, void *buf, size_t maxline);
+
+void send_fd(int sock_fd, int fd);
+int recv_fd(const int sock_fd);
+
+
+#endif
